6f0d69de7f0acc286fdaae3a0113b822561a200e
LucasBoschelli <lucas.boschelli@gmail.com>
2019-08-07 14:52:27 -0500

    Update lab02.py
    Drastically simplified clock and functions

--- day02/lab02.py
+++ day02/lab02.py
@@ -5,50 +5,32 @@ class Clock:
     def __init__(self, hour, minutes):
         self.minutes = minutes + 60*hour
         self.hour = hour
-
     ## Print the time
     def __str__(self):
-        # Creates initial time string
+        # Calculates hours and minutes for output string
         hours=self.minutes//60
         minutes=self.minutes-(hours*60)
-        time="%d:%02d"%(hours,minutes)
-        # Returns formatted time
-        return ("The time is %s"%time)
-
-
+        return ("The time is %d:%02d"%(hours,minutes))
     ## Add time
-    ## Don't return anything
     def __add__(self,minutes):
         self.minutes+=minutes
-            # Checks if minutes overflow
+        # Checks for minute overflow
         if self.minutes > 1440:
-                # Sets the number of times minutes overflow (go over 60)
-            m=self.minutes//1440
-                # Adjusts minutes
-            self.minutes-=1440*m
-
-
+            self.minutes-=1440*(self.minutes//1440)
     ## Subtract time
-    ## Don't return anything
     def __sub__(self,minutes):
         self.minutes-=minutes
+        # Checks for minute underflow
         if self.minutes < 0:
-            # Sets number of times minutes underflow (go under 0 in units of 60)
-            m=abs(self.minutes)//1440
-            # Adjusts minutes for underflow
-            self.minutes+=1440*m
-            # Turns negative minutes into positive form
-            self.minutes=1440+self.minutes
-
+            self.minutes=1440+(self.minutes+1440*(abs(self.minutes)//1440))
     ## Are two times equal?
     def __eq__(self, other):
-        if self.minutes == other.minutes and self.hour == other.hour:
+        if self.minutes == other.minutes:
             return True
         return False
-
     ## Are two times not equal?
     def __ne__(self, other):
-        if self.minutes != other.minutes or self.hour != other.hour:
+        if self.minutes != other.minutes:
             return True
         return False
 
