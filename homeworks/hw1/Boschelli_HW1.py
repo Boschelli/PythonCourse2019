7c1425b71f59be61b7ff8a739aaa4402bfe83c3e
LucasBoschelli <lucas.boschelli@gmail.com>
2019-08-07 16:23:03 -0500

    Update Boschelli_HW1.py
    Finished up buy and sell stock functions

--- homeworks/hw1/Boschelli_HW1.py
+++ homeworks/hw1/Boschelli_HW1.py
@@ -1,36 +1,43 @@
 import random
 
-
-
-test={"Stock":{"Test":1},"MF":{}}
-test["Stock"]["Test"]
-
-
 class Portfollio():
-    def__init__(self, name, cash=0):
-        self.name=name
+    def __init__(self, cash=0):
         self.cash=cash
         self.items={"Stock":{},"MF":{}}
         self.log=[]
     def addCash(self,cash):
         self.cash+=cash
 
-    def subCash(self.cash):
+    def subCash(self,cash):
         #MAKE SURE NO OVER DRAFT
         self.cash-=cash
 
-    def buyStock(quantity,stock):
-        if cash<quantity*stock.value:
+    def buyStock(self,quantity,stock):
+        if type(quantity)!=int:
+            return "Error Stocks Can Only Be Purchased As Whole Units"
+        if self.cash<quantity*stock.value:
             return "Error Not Enough Funds"
         if stock.symbol in self.items["Stock"]:
             self.items["Stock"][stock.symbol]+=1
         else: self.items["Stock"][stock.symbol]=1
-        self.log.append("Bought %s for $d"%(stock.symbol,stock.value))
+        self.cash-=quantity*stock.value
+        self.log.append("Bought %s for %d"%(stock.symbol,stock.value))
 
+    def sellStock(self,quantity,stock):
+        if type(quantity)!=int:
+            return "Error Stocks Can Only Be Purchased As Whole Units"
+        if stock.symbol not in self.items["Stock"]:
+            return "You Don't Own Any %s Stock"%stock.symbol
+        if quantity > self.items["Stock"][stock.symbol]:
+            return "You only own %d of %s"%(self.items["Stock"][stock.symbol],stock.symbol)
+        self.items["Stock"][stock.symbol]-=1
+        sellValue=quantity*stock.sell
+        self.cash+=sellValue
+        self.log.append("Sold %s for %d"%(stock.symbol,sellValue))
 
 
 class Financialitem():
-    def__init__(self,value, symbol):
+    def __init__(self,value, symbol):
         self.value=value
         self.symbol=symbol
     def sell(self):
@@ -42,8 +49,14 @@ class Stock(Financialitem):
         return random.uniform(0.5*self.value,1.5*self.value)
 
 class MF(Financialitem):
-    def__init__(self,value=1,symbol)
-        self.value=value
+    def __init__(self,symbol):
+        self.value=1
         self.symbol=symbol
     def sell(self):
         return random.uniform(0.9-1.2)
+
+portfollio = Portfollio()
+portfollio.addCash(300.50)
+s = Stock(20,"HFH")
+portfollio.buyStock(5,s)
+portfollio.log
