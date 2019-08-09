306098e7fe42494c37130222c9d5fbebfc8eb461
LucasBoschelli <lucas.boschelli@gmail.com>
2019-08-09 15:00:47 -0500

    Finished Lab
    -finished lab
    -finished lab tester

--- day03/lab03.py
+++ day03/lab03.py
@@ -8,26 +8,63 @@ import string
 ## Presumably your code won't work for an int
 ## raise a built-in (or custom!) exception if fed an int
 
-
 ## make all characters capitalized
 def shout(txt):
+	try:
+		result = txt.upper()
+	except AttributeError:
+		print("Be Sure To Only Enter A String")
+	return txt.upper()
 
 
 ## reverse all characters in string
 def reverse(txt):
+	if type(txt)!=str:
+		raise TypeError("Be Sure To Only Enter A String")
+	result=""
+	for i in txt:
+		result=i+result
+	return result
 
 
 ## reverse word order in string
 def reversewords(txt):
+	if type(txt)!=str:
+		raise TypeError("Be Sure To Only Enter A String")
+	result=""
+	for word in txt.split():
+		result=word+" "+result
+	return result
 
 
 ## reverses letters in each word
 def reversewordletters(txt):
+	if type(txt)!=str:
+		raise TypeError("Be Sure To Only Enter A String")
+	result=""
+	for word in txt.split():
+		result+=reverse(word)+ " "
+	return result
 
-		
-## change text to piglatin.. google it! 
+
+## change text to piglatin.. google it!
 def piglatin(txt):
-
+	if type(txt)!=str:
+		raise TypeError("Be Sure To Only Enter A String")
+	vowels=['a','e','i','o','u','y']
+	result=""
+	for word in txt.split():
+		newWord=""
+		infront=""
+		for i in range(0,len(word)):
+			if word[i].lower() in vowels:
+				infront+='ay'
+				newWord=word[i:]
+				newWord+=infront
+				break
+			infront+=word[i]
+		result+=newWord + " "
+	return result
 
 
 
@@ -37,15 +74,10 @@ def piglatin(txt):
 ## Loop over this string and apply the reverse() function.
 ## Should throw errors if your exceptions are being raised!
 ## Write a try/catch to handle this.
- 
 string_list = ["hi", "hello there", 5, "hope this works", 100, "will it?"]
 
-
-		
-			
-			
-			
-			
-			
-			
-
+for i in string_list:
+	try:
+		print(reverse(i))
+	except:
+		continue
--- day03/lab03_test.py
+++ day03/lab03_test.py
@@ -2,21 +2,21 @@ import unittest
 from lab03 import *
 
 class labTests(unittest.TestCase):
-	
-	## fill in a few tests for each
-	## make sure to account for correct and incorrect input
 
-    def test_shout(self):
-
-    def test_reverse(self):
-
-    def test_reversewords(self):
-
-    def test_reversewordletters(self):
-
-    def test_piglatin(self):
-
+  def test_shout(self):
+    self.assertEqual("TEST",shout("test"))
+  def test_reverse(self):
+    self.assertEqual("tset",reverse("test"))
+    with self.assertRaises(TypeError): reverse(67)
+  def test_reversewords(self):
+    self.assertEqual("Lucas Is Name My Hello ",reversewords("Hello My Name Is Lucas"))
+    with self.assertRaises(TypeError): reversewords([3,5,6,"Hello"])
+  def test_reversewordletters(self):
+    self.assertEqual("olleH yM emaN sI sacuL ",reversewordletters("Hello My Name Is Lucas"))
+    with self.assertRaises(TypeError): reversewordletters([3,5,6,"Hello"])
+  def test_piglatin(self):
+    self.assertEqual('isThay isay aay esttay ',piglatin("This is a test"))
+    with self.assertRaises(TypeError): reverse(54234)
 
 if __name__ == '__main__':
   unittest.main()
-
